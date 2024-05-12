eba9ec7571970f8369f81df2a48075bafc1629ac
KimJiseon <reme8267@gmail.com>
2024-05-12 20:50:11 +0900

    default pybo api complete

--- domain/question/question_schema.py
+++ domain/question/question_schema.py
@@ -3,6 +3,7 @@ import datetime
 from pydantic import BaseModel, validator
 
 from domain.answer.answer_schema import Answer
+from domain.user.user_schema import User
 
 
 class Question(BaseModel):
@@ -11,6 +12,10 @@ class Question(BaseModel):
     content: str
     create_date: datetime.datetime
     answers: list[Answer] = []
+    user: User | None
+    modify_date: datetime.datetime | None = None
+    voter: list[User] = []
+
 
     class Config:
         orm_mode = True
@@ -25,9 +30,20 @@ class QuestionCreate(BaseModel):
         if not v or not v.strip():
             raise ValueError('빈 값은 허용되지 않습니다.')
         return v
-    
+
 
 class QuestionList(BaseModel):
     total: int = 0
     question_list: list[Question] = []
     
+    
+class QuestionUpdate(QuestionCreate):
+    question_id: int
+
+
+class QuestionDelete(BaseModel):
+    question_id: int
+    
+
+class QuestionVote(BaseModel):
+    question_id: int
